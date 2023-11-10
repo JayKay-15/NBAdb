4ef4042e868a318a003a91cf477dd6b3eb0cbbef
Jesse Kartes <jesse.kartes@gmail.com>
2023-11-10 15:02:03 -0600

    Update hoopR_db.R

--- code/hoopR_db.R
+++ code/hoopR_db.R
@@ -75,7 +75,7 @@ generate_parameters <- function(year, measure_type) {
     return(params)
 }
 
-nba_schedule_bridge <- function(seasons) {
+scrape_nba_odds <- function(seasons) {
     headers <- generate_headers()
     all_data <- data.frame()
     
@@ -93,100 +93,102 @@ nba_schedule_bridge <- function(seasons) {
         print(paste0(params$Season, " ", params$MeasureType))
     }
     
-    return(all_data)
+    statr_schedule <- all_data %>%
+        clean_names() %>%
+        arrange(game_date, game_id) %>%
+        mutate(location = if_else(grepl("@", matchup) == T, "away", "home"),
+               game_date = as_date(game_date),
+               season_year = as.numeric(substr(season_year, 1, 4)) + 1,
+               team_name = str_replace_all(team_name,
+                                           "Los Angeles Clippers", "LA Clippers")) %>%
+        select(season_year, game_id, game_date, location, team_name) %>%
+        filter(location == "away") %>%
+        arrange(game_id)
+    
+    hoopr_schedule <- as.data.frame(hoopR::load_nba_schedule(seasons = 2024))
+    hoopr_schedule <- hoopr_schedule %>%
+        filter(type_id == 1) %>%
+        select(id, date, away_display_name) %>%
+        mutate(date = ymd_hm(gsub("T|Z", " ", date), tz = "UTC"),
+               date = date(with_tz(date, tzone = "America/New_York")))
+    
+    nba_bridge <- statr_schedule %>%
+        left_join(hoopr_schedule, by = c("game_date" = "date",
+                                         "team_name" = "away_display_name")) %>%
+        rename(hoopr_id = id)
+    
+    # check missing ids
+    nba_bridge_missing <- nba_bridge %>%
+        filter(is.na(hoopr_id))
+    
+    print(paste0("hoopR games missing: ", length(nba_bridge_missing$hoopr_id)))
+    
+    ids <- unique(nba_bridge$hoopr_id)
+    
+    # initialize an empty list to store odds data frames
+    odds_list <- list()
+    
+    # define the function to collect odds for a single id
+    collect_odds <- function(i) {
+        tryCatch({
+            odds_list <- hoopR::espn_nba_betting(i)
+            odds_picks <- odds_list$pickcenter
+            
+            odds <- odds_picks %>%
+                mutate(game_id = i) %>%
+                select(game_id, provider_name, spread, over_under,
+                       away_team_odds_team_id, home_team_odds_team_id,
+                       away_team_odds_money_line, home_team_odds_money_line)
+            
+            print(paste0("Collected odds for ", i))
+            
+            return(odds)
+        }, error = function(e) {
+            cat("Error occurred for id:", i, "- Skipping.\n")
+            return(NULL) # return NULL to indicate that there was an error
+        })
+    }
+    
+    # use lapply to collect odds for each id and store them in the odds_list
+    odds_list <- lapply(ids, collect_odds)
+    
+    # filter out NULL elements which correspond to errors
+    odds_list <- odds_list[sapply(odds_list, function(x) !is.null(x))]
+    
+    # combine the individual data frames into one using bind_rows
+    odds_df <- bind_rows(odds_list)
+    
+    # Transform odds data
+    odds_clean <- odds_df %>%
+        filter(provider_name == "ESPN Bet") %>%
+        mutate(away_spread = -spread) %>%
+        rename(hoopr_id = game_id,
+               home_spread = spread,
+               away_moneyline = away_team_odds_money_line,
+               home_moneyline = home_team_odds_money_line) %>%
+        select(hoopr_id, away_spread, home_spread, away_moneyline, home_moneyline,
+               over_under)
+    
+    odds_wpo <- odds_clean %>%
+        mutate(away_moneyline = odds.converter::odds.us2dec(away_moneyline),
+               home_moneyline = odds.converter::odds.us2dec(home_moneyline)) %>%
+        select(away_moneyline, home_moneyline)
+    
+    odds_wpo <- implied::implied_probabilities(odds_wpo, method = 'wpo')
+    
+    odds_final <- odds_clean %>%
+        mutate(away_implied_prob = odds_wpo$probabilities[,1],
+               home_implied_prob = odds_wpo$probabilities[,2])
+    
+    # attach odds
+    odds_db <- nba_bridge %>%
+        left_join(odds_final) %>%
+        arrange(game_date)
+    
+    return(odds_db)
 }
 
-all_data <- as.data.frame(nba_schedule_bridge(seasons = 2024))
-
-statr_schedule <- all_data %>%
-    clean_names() %>%
-    arrange(game_date, game_id) %>%
-    mutate(location = if_else(grepl("@", matchup) == T, "away", "home"),
-           game_date = as_date(game_date),
-           season_year = as.numeric(substr(season_year, 1, 4)) + 1,
-           team_name = str_replace_all(team_name,
-                                       "Los Angeles Clippers", "LA Clippers")) %>%
-    select(season_year, game_id, game_date, location, team_name) %>%
-    filter(location == "away") %>%
-    arrange(game_id)
-
-hoopr_schedule <- as.data.frame(hoopR::load_nba_schedule(seasons = 2024))
-hoopr_schedule <- hoopr_schedule %>%
-    filter(type_id == 1) %>%
-    select(id, date, away_display_name) %>%
-    mutate(date = ymd_hm(gsub("T|Z", " ", date), tz = "UTC"),
-           date = date(with_tz(date, tzone = "America/New_York")))
-
-nba_bridge <- statr_schedule %>%
-    left_join(hoopr_schedule, by = c("game_date" = "date",
-                                     "team_name" = "away_display_name")) %>%
-    rename(hoopr_id = id)
-
-# check missing ids
-nba_bridge_missing <- nba_bridge %>%
-    filter(is.na(hoopr_id))
-
-ids <- unique(nba_bridge$hoopr_id)
-
-# initialize an empty list to store odds data frames
-odds_list <- list()
-
-# define the function to collect odds for a single id
-collect_odds <- function(i) {
-    tryCatch({
-        odds_list <- hoopR::espn_nba_betting(i)
-        odds_picks <- odds_list$pickcenter
-        
-        odds <- odds_picks %>%
-            mutate(game_id = i) %>%
-            select(game_id, provider_name, spread, over_under,
-                   away_team_odds_team_id, home_team_odds_team_id,
-                   away_team_odds_money_line, home_team_odds_money_line)
-        
-        print(paste0("Collected odds for ", i))
-        
-        return(odds)
-    }, error = function(e) {
-        cat("Error occurred for id:", i, "- Skipping.\n")
-        return(NULL) # return NULL to indicate that there was an error
-    })
-}
-
-# use lapply to collect odds for each id and store them in the odds_list
-odds_list <- lapply(ids, collect_odds)
-
-# filter out NULL elements which correspond to errors
-odds_list <- odds_list[sapply(odds_list, function(x) !is.null(x))]
-
-# combine the individual data frames into one using bind_rows
-odds_df <- bind_rows(odds_list)
-
-# Transform odds data
-odds_clean <- odds_df %>%
-    filter(provider_name == "ESPN Bet") %>%
-    mutate(away_spread = -spread) %>%
-    rename(hoopr_id = game_id,
-           home_spread = spread,
-           away_moneyline = away_team_odds_money_line,
-           home_moneyline = home_team_odds_money_line) %>%
-    select(hoopr_id, away_spread, home_spread, away_moneyline, home_moneyline,
-           over_under)
-
-odds_wpo <- odds_clean %>%
-    mutate(away_moneyline = odds.converter::odds.us2dec(away_moneyline),
-           home_moneyline = odds.converter::odds.us2dec(home_moneyline)) %>%
-    select(away_moneyline, home_moneyline)
-
-odds_wpo <- implied::implied_probabilities(odds_wpo, method = 'wpo')
-
-odds_final <- odds_clean %>%
-    mutate(away_implied_prob = odds_wpo$probabilities[,1],
-           home_implied_prob = odds_wpo$probabilities[,2])
-
-# attach odds
-odds_db <- nba_bridge %>%
-    left_join(odds_final) %>%
-    arrange(game_date)
+odds_db <- scrape_nba_odds(2024)
 
 odds_db_missing <- odds_db %>%
     filter(is.na(away_spread))
