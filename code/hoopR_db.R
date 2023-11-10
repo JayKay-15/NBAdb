7d741dce9253d521da74ba9a28100324452273dc
Jesse Kartes <jesse.kartes@gmail.com>
2023-11-10 12:49:31 -0600

    Update hoopR_db.R

--- code/hoopR_db.R
+++ code/hoopR_db.R
@@ -96,7 +96,7 @@ nba_schedule_bridge <- function(seasons) {
     return(all_data)
 }
 
-all_data <- as.data.frame(nba_schedule_bridge(seasons = 2014:2023))
+all_data <- as.data.frame(nba_schedule_bridge(seasons = 2024))
 
 statr_schedule <- all_data %>%
     clean_names() %>%
@@ -110,7 +110,7 @@ statr_schedule <- all_data %>%
     filter(location == "away") %>%
     arrange(game_id)
 
-hoopr_schedule <- as.data.frame(hoopR::load_nba_schedule(seasons = 2014:2023))
+hoopr_schedule <- as.data.frame(hoopR::load_nba_schedule(seasons = 2024))
 hoopr_schedule <- hoopr_schedule %>%
     filter(type_id == 1) %>%
     select(id, date, away_display_name) %>%
@@ -122,70 +122,9 @@ nba_bridge <- statr_schedule %>%
                                      "team_name" = "away_display_name")) %>%
     rename(hoopr_id = id)
 
-# nba_bridge_missing <- nba_bridge %>%
-#     filter(is.na(hoopr_id))
-
-saveRDS(nba_bridge, "./nba_bridge.rds")
-
-# attach odds
-odds_db <- nba_bridge %>%
-    left_join(odds_final)
-
-saveRDS(odds_db, "./nba_odds.rds")
-
-missing_odds <- odds_db %>%
-    filter(is.na(away_spread))
-
-saveRDS(missing_odds, "./missing_odds.rds")
-
-
-NBAdb <- DBI::dbConnect(RSQLite::SQLite(), "../nba_sql_db/nba_db")
-DBI::dbWriteTable(NBAdb, "nba_odds", odds_db, append = T)
-DBI::dbDisconnect(NBAdb)
-
-
-df <- tbl(dbConnect(SQLite(), "../nba_sql_db/nba_db"), "nba_odds") %>%
-    collect() %>%
-    mutate(game_date = as_date(game_date, origin ="1970-01-01"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-all_data <- as.data.frame(nba_schedule_bridge(seasons = 2014:2023))
-
-statr_schedule <- all_data %>%
-    clean_names() %>%
-    arrange(game_date, game_id) %>%
-    mutate(location = if_else(grepl("@", matchup) == T, "away", "home"),
-           game_date = as_date(game_date),
-           season_year = as.numeric(substr(season_year, 1, 4)) + 1,
-           team_name = str_replace_all(team_name,
-                                       "Los Angeles Clippers", "LA Clippers")) %>%
-    select(season_year, game_id, game_date, location, team_name) %>%
-    filter(location == "away") %>%
-    arrange(game_id)
-
-hoopr_schedule <- as.data.frame(hoopR::load_nba_schedule(seasons = 2014:2023))
-hoopr_schedule <- hoopr_schedule %>%
-    filter(type_id == 1) %>%
-    select(id, date, away_display_name) %>%
-    mutate(date = ymd_hm(gsub("T|Z", " ", date), tz = "UTC"),
-           date = date(with_tz(date, tzone = "America/New_York")))
-
-nba_bridge <- statr_schedule %>%
-    left_join(hoopr_schedule, by = c("game_date" = "date",
-                                     "team_name" = "away_display_name")) %>%
-    rename(hoopr_id = id)
+# check missing ids
+nba_bridge_missing <- nba_bridge %>%
+    filter(is.na(hoopr_id))
 
 ids <- unique(nba_bridge$hoopr_id)
 
@@ -222,7 +161,6 @@ odds_list <- odds_list[sapply(odds_list, function(x) !is.null(x))]
 # combine the individual data frames into one using bind_rows
 odds_df <- bind_rows(odds_list)
 
-
 # Transform odds data
 odds_clean <- odds_df %>%
     filter(provider_name == "ESPN Bet") %>%
@@ -249,20 +187,83 @@ odds_final <- odds_clean %>%
 odds_db <- nba_bridge %>%
     left_join(odds_final) %>%
     arrange(game_date)
-    
+
 odds_db_missing <- odds_db %>%
     filter(is.na(away_spread))
 
+saveRDS(odds_db, "./nba_odds_2024.rds")
+
+saveRDS(odds_db_missing, "./missing_odds_2024.rds")
+
+# NBAdb <- DBI::dbConnect(RSQLite::SQLite(), "../nba_sql_db/nba_db")
+# DBI::dbWriteTable(NBAdb, "nba_odds", odds_db, append = T)
+# DBI::dbDisconnect(NBAdb)
+
+odds_db <- tbl(dbConnect(SQLite(), "../nba_sql_db/nba_db"), "nba_odds") %>%
+    collect() %>%
+    mutate(game_date = as_date(game_date, origin ="1970-01-01"))
 
 
 
 
+#### fixes missing odds ----
+odds_db_missing <- read_rds("./missing_odds.rds")
+
+new_odds <- odds_db_missing
+
+lookup_table <- data.frame(
+    game_id = c(odds_db_missing$game_id),
+    away_spread = c(-4.5, -12.5, 3, 0, 6.5,
+                    6.5, 5.5, -1.5, 9.5, -7,
+                    -6.5, 14.5, 8, -7.5, -5,
+                    6.5, 4.5, 7.5),
+    home_spread = c(4.5, 12.5, -3, 0, -6.5,
+                    -6.5, -5.5, 1.5, -9.5, 7,
+                    6.5, -14.5, -8, 7.5, 5,
+                    -6.5, -4.5, -7.5),
+    away_moneyline = c(-190, -1130, 135, -110, 240,
+                       220, 190, -130, 400, -290,
+                       -280, 1100, 310, -325, -220,
+                       230, 155, 270),
+    home_moneyline = c(165, 730, -155, -110, -290,
+                       -260, -230, 110, -525, 240,
+                       230, -2500, -380, 260, 180,
+                       -280, -175, -330),
+    over_under = c(186.5, 228, 213, 206, 203.5,
+                   214, 208.5, 198, 211, 194,
+                   195.5, 187.5, 192, 201, 207,
+                   218.5, 216.5, 210.5)
+)
 
 
+new_odds <- odds_db_missing %>%
+    left_join(lookup_table, by = "game_id", suffix = c("", "_lookup")) %>%
+    mutate(
+        away_spread = coalesce(away_spread, away_spread_lookup),
+        home_spread = coalesce(home_spread, home_spread_lookup),
+        away_moneyline = coalesce(away_moneyline, away_moneyline_lookup),
+        home_moneyline = coalesce(home_moneyline, home_moneyline_lookup),
+        over_under = coalesce(over_under, over_under_lookup)
+    ) %>%
+    select(-ends_with("_lookup"))
 
+odds_wpo <- new_odds %>%
+    mutate(away_moneyline = odds.converter::odds.us2dec(away_moneyline),
+           home_moneyline = odds.converter::odds.us2dec(home_moneyline)) %>%
+    select(away_moneyline, home_moneyline)
 
+odds_wpo <- implied::implied_probabilities(odds_wpo, method = 'wpo')
 
+odds_db <- new_odds %>%
+    mutate(away_implied_prob = odds_wpo$probabilities[,1],
+           home_implied_prob = odds_wpo$probabilities[,2],
+           season_year = c(2014, 2014, 2014, 2014, 2014, 2014, 2014, 2014, 2014,
+                           2015, 2015, 2015, 2016, 2016, 2017, 2018, 2021, 2021)) %>%
+    select(season_year, game_id:home_implied_prob)
 
+df_check <- df %>%
+    group_by(season_year) %>%
+    tally()
 
 
 
